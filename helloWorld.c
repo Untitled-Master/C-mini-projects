0a4e881c82838be4eb4f9bc7711fdcb179cdaae3
Ahmed Belmehnouf <128633214+Untitled-Master@users.noreply.github.com>
2024-09-09 19:00:12 +0100

    Create helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,11 @@
+// Write a simple C program that prints "Hello World!" to the screen.
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World");
+
+    return 0;
+}
+
+// Output: Hello World
